05a36b80df785833b36bf6830c47ad3e5c4fc9c7
Gabriele Falloon <ggfalloon@gmail.com>
2020-12-08 11:31:32 -0600

    formspree for connect card

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+function resetForm() {
+    document.getElementById("myForm").reset();
+}
+
 $(document).ready(function () {
+
+    $("button").click(function () {
+        $("#myForm").trigger("reset");
+    });
+
     $('[data-toggle="tooltip"]').tooltip();
 
 });
