e25ccd5587823302ea283b6e52ee45a3dd0d5614
Gabriele Falloon <ggfalloon@gmail.com>
2020-12-14 15:16:57 -0600

    added desciptions to website links

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// function resetForm() {
-//     document.getElementById("myForm").reset();
-// }
-
 $(document).ready(function () {
-
-    // $("button").click(function () {
-    //     $("#myForm").trigger("reset");
-    // });
 
     $('[data-toggle="tooltip"]').tooltip();
 
